2d37ceecc7e4136b24d3d8e11e931f5e3a0584ea
Mikhail <kd.maikl-201@yandex.ru>
2021-06-14 14:03:23 +0700

    add commit and  change import package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_task_layout/src/MyApp.dart';
+import 'package:test_task_layout/src/my_app.dart';
 
+//application entry point
 void main() => runApp(MyApp());
