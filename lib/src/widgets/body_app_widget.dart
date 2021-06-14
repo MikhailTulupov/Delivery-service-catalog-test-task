be1377ff1d859cfefcd7c921348735d820a7e4d1
Mikhail <kd.maikl-201@yandex.ru>
2021-06-14 14:06:26 +0700

    create body from main widget

---
+++ lib/src/widgets/body_app_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:test_task_layout/src/widgets/menu_nav.dart';
+
+// body widget app
+class MainContainer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+         MenuCategory()
+        ],
+      ),
+    );
+  }
+}
