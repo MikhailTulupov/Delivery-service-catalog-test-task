e6596edbba296b2003636b35fa7f98323b49cdc5
Mikhail <kd.maikl-201@yandex.ru>
2021-06-14 18:08:17 +0700

    some fixes

--- lib/src/widgets/action_bar.dart
+++ lib/src/widgets/action_bar.dart
@@ -1,33 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:test_task_layout/src/values/dimens.dart';
 
 final String _title = 'Бургер Кинг';
+// ignore: non_constant_identifier_names
+final double _icon_size_large_32 = 32;
 
 // actionBar widget
 PreferredSizeWidget actionBar = AppBar(
-      backgroundColor: Colors.black,
-      centerTitle: true,
-      leading: Icon(
-        Icons.arrow_back,
+  backgroundColor: Colors.black,
+  centerTitle: true,
+  leading: Icon(
+    Icons.arrow_back,
+    color: Colors.white,
+    size: _icon_size_large_32,
+  ),
+  title: Text(
+    _title, // data
+    style: TextStyle(
+        color: Colors.white, fontSize: 24, fontWeight: FontWeight.w500),
+  ),
+  actions: [
+    Padding(
+      padding: EdgeInsets.only(right: 10),
+      child: Icon(
+        Icons.info_outline_rounded,
         color: Colors.white,
-        size: icon_size_large_32,
+        size: _icon_size_large_32,
       ),
-      title: Text(
-        _title, // data
-        style: TextStyle(
-            color: Colors.white, 
-            fontSize: font_size_normal_24, 
-            fontWeight: FontWeight.w500),
-      ),
-      actions: [
-        Padding(
-          padding: EdgeInsets.only(right: 10),
-          child: Icon(
-            Icons.info_outline_rounded,
-            color: Colors.white,
-            size: icon_size_large_32,
-          ),
-        )
-      ],
-    );
+    )
+  ],
+);
